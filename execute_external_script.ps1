ac5d500e0a8118ed4f066a85dfe52ded4572c442
Kevin Smith <kev_bite@msn.com>
2015-09-18 16:47:40 +0000

    execute_external_script.ps1 edited online with Bitbucket

--- execute_external_script.ps1
+++ execute_external_script.ps1
@@ -5,6 +5,6 @@ Function ExecuteExternalScript([string]$url, [hashtable]$data)
 
 	Write-Output "Creating script block with params"
 	$data | Out-String | Write-Host
-	$scriptBlock = [scriptblock]::create(".{$scriptString} $(&{$args} @params)")
+	$scriptBlock = [scriptblock]::create(".{$scriptString} $(&{$args} @data)")
 	Invoke-Command -ScriptBlock $scriptBlock
 }
