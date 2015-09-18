88e6ad988a308d3f34223110f600966e8a7df59b
Kevin Smith <kev_bite@msn.com>
2015-09-18 16:43:50 +0000

    execute_external_script.ps1 edited online with Bitbucket

--- execute_external_script.ps1
+++ execute_external_script.ps1
@@ -1,10 +1,10 @@
-Function ExecuteExternalScript([string]$url, [hashtable]$params)
+Function ExecuteExternalScript([string]$url, [hashtable]$data)
 {
 	Write-Output "Downloading script $url"
-	$replace_appsettings = (new-object net.webclient).DownloadString($url)
+	$scriptString = (new-object net.webclient).DownloadString($url)
 
 	Write-Output "Creating script block with params"
-	$params | Out-String | Write-Host
-	$scriptBlock = [scriptblock]::create(".{$replace_appsettings} $(&{$params} @params)")
+	$data | Out-String | Write-Host
+	$scriptBlock = [scriptblock]::create(".{$scriptString} $(&{$data} @params)")
 	Invoke-Command -ScriptBlock $scriptBlock
 }
