aa15b57640ac65225e822129dfa699093a962775
Matt Wheatley <terrawheat@gmail.com>
2015-12-23 09:18:04 +0000

    Update slack script to work for Dev and Live

--- notify_slack_deployment.ps1
+++ notify_slack_deployment.ps1
@@ -1,12 +1,13 @@
 $EnvTag = [Environment]::GetEnvironmentVariable("Liberis:Environment.Name");
 
-$url = "https://hooks.slack.com/services/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";
+$urls = @{
+  "LIVE" = "https://hooks.slack.com/services/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";
+  "DEV" = "https://hooks.slack.com/services/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";
+};
 
 $payload = @{
   "text" = "[$EnvTag] - Deployed: $env:APPLICATION_NAME - v$env:APPVEYOR_BUILD_VERSION";
 } | ConvertTo-Json;
 
+Invoke-RestMethod $urls.$EnvTag -Method Post -Body $payload -ContentType 'application/json';
 
-If ($EnvTag -eq "LIVE") {
-  Invoke-RestMethod $url -Method Post -Body $payload -ContentType 'application/json';
-}
